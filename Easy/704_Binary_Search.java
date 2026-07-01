42aee05a340c8a26f7fd84b0caf97d31f2767919
Vikas-TK <vikastk.9b@gmail.com>
2026-07-01 22:23:25 +0530

    ADDED 01-07-26

---
+++ Easy/704_Binary_Search.java
@@ -0,0 +1,14 @@
+int search(int* nums, int numsSize, int target) {
+    int l=0,h=numsSize-1;
+    while(l<=h){
+        int m=(l+h)/2;
+        if(nums[m]==target){
+            return m;
+        }else if(nums[m]<target){
+            l=m+1;
+        }else{
+            h=m-1;
+        }
+    }
+    return -1;
+}
